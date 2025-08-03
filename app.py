b175d4f5792ff5dcbc974c202b11668d39e65297
sakibtalukqder <sakibtalukqder07@gmail.com>
2025-08-03 13:01:40 +0600

    Linting Error Solved

--- app.py
+++ app.py
@@ -1,19 +1,19 @@
 """A simple script to calculate factorial using recursion."""
 
 def factorial(n: int) -> int:
-        """
+    """
     Calculate the factorial of a given number using recursion.
     """
     if n in (0, 1):
-            return 1
+        return 1
     return n * factorial(n - 1)
 
 def sumf(a: int,b: int) -> int:
-        """
-        Calculate Summation of tow Numbers.
-        """
-            summation = a +b
-        return summation
+    """
+    Calculate Summation of tow Numbers.
+    """
+    summation = a +b
+    return summation
 
 def main() -> None:
     """Main function to take user input and display the factorial."""
