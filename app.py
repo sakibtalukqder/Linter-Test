84a43a619d97830cbb26b0cb8dd0dd89264272e3
sakibtalukqder <sakibtalukqder07@gmail.com>
2025-08-06 13:13:38 +0600

    Flask Init 02

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+"""
+A simple Flask app that returns 'Hello World' at http://localhost:3300
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """
+    Handle the root route and return a simple 'Hello World' message.
+    """
     return "Hello, World!"
 
 if __name__ == '__main__':
